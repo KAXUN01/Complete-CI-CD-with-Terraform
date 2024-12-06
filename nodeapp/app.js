110fa8831f1e2dabd1a860d28197f4b4318745b6
Kasun madhushan wickramasinghe <119012876+KAXUN01@users.noreply.github.com>
2024-12-07 00:50:09 +0530

    added configurations

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("My Server is up and running");
+  res.send("My Server is up and running.");
 });
 
 app.listen(8080, () => {
